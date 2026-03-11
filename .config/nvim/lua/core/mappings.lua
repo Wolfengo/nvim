d3219fa0eac5465d94a87bd37b8b9064199c293d
geo <geo@geo.wolfengo.online>
2026-03-11 12:31:46 +0300

    qq not close window

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -22,6 +22,31 @@ local function jump_syntax_block(direction)
     vim.cmd(direction == "next" and "normal! }" or "normal! {")
 end
 
+local function close_current_buffer()
+    local current = vim.api.nvim_get_current_buf()
+    local listed = vim.tbl_filter(function(buf)
+        return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+    end, vim.api.nvim_list_bufs())
+
+    if #listed <= 1 then
+        vim.cmd("enew")
+        vim.cmd("bdelete " .. current)
+        return
+    end
+
+    local alternate = vim.fn.bufnr("#")
+    if alternate > 0 and vim.api.nvim_buf_is_loaded(alternate) and vim.bo[alternate].buflisted then
+        vim.api.nvim_set_current_buf(alternate)
+    else
+        vim.cmd("bnext")
+        if vim.api.nvim_get_current_buf() == current then
+            vim.cmd("bprevious")
+        end
+    end
+
+    vim.cmd("bdelete " .. current)
+end
+
 vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree git_status float<CR>')
 
@@ -38,7 +63,7 @@ vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
 vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
 vim.keymap.set('n', 'ww', ':w<CR>')
-vim.keymap.set('n', 'qq', ':bd<CR>')
+vim.keymap.set('n', 'qq', close_current_buffer)
 vim.keymap.set('n', '<M-j>', function() jump_syntax_block("next") end)
 vim.keymap.set('n', '<M-k>', function() jump_syntax_block("prev") end)
 vim.keymap.set('i', 'jj', '<Esc>')
